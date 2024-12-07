3619a26b37a54a12d6e5f90841d069d307365d34
Debojyotibabai <debojyoti@appstango.com>
2024-12-07 13:05:11 +0530

    add loader in details screen

--- lib/features/recommendation_details/presentation/pages/recommendation_details_page.dart
+++ lib/features/recommendation_details/presentation/pages/recommendation_details_page.dart
@@ -4,6 +4,7 @@ import 'package:clean_architecture_rivaan_ranawat/features/recommendation_detail
 import 'package:clean_architecture_rivaan_ranawat/utils/widgets/image_banner_scroll_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class RecommendationDetailsScreen extends StatefulWidget {
   const RecommendationDetailsScreen({
@@ -34,99 +35,113 @@ class _RecommendationDetailsScreenState
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const ImageBannerScrollView(),
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
+        child:
+            BlocBuilder<RecommendationDetailsBloc, RecommendationDetailsState>(
+          builder: (context, state) {
+            if (state is RecommendationDetailsIsLoading) {
+              return Center(
+                child: SpinKitThreeInOut(
+                  color: Colors.yellow[700],
+                ),
+              );
+            } else if (state is RecommendationDetailsSuccess) {
+              return Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  const Text(
-                    "Taco Bell",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                      fontSize: 25,
+                  const ImageBannerScrollView(),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Taco Bell",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 25,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.phone,
+                              size: 17,
+                              color: Colors.red[900],
+                            ),
+                            const SizedBox(width: 10),
+                            Text(
+                              "(801) 123-1234",
+                              style: TextStyle(
+                                color: Colors.red[900],
+                                fontSize: 17,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.location_pin,
+                              size: 17,
+                              color: Colors.grey,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              "571 N State St, Lindon, UT 84042",
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 17,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
-                  const SizedBox(height: 8),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Icon(
-                        Icons.phone,
-                        size: 17,
-                        color: Colors.red[900],
+                      TabOption(
+                        title: "Recommended",
+                        selectedTab: selectedTab == 1,
+                        onTap: () => setState(() => selectedTab = 1),
                       ),
-                      const SizedBox(width: 10),
-                      Text(
-                        "(801) 123-1234",
-                        style: TextStyle(
-                          color: Colors.red[900],
-                          fontSize: 17,
-                          fontWeight: FontWeight.w500,
-                        ),
+                      TabOption(
+                        title: "All Menu Items",
+                        selectedTab: selectedTab == 2,
+                        onTap: () => setState(() => selectedTab = 2),
                       ),
                     ],
                   ),
-                  const SizedBox(height: 10),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.location_pin,
-                        size: 17,
-                        color: Colors.grey,
-                      ),
-                      SizedBox(width: 10),
-                      Text(
-                        "571 N State St, Lindon, UT 84042",
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 17,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
+                  const SizedBox(height: 20),
+                  Expanded(
+                    child: selectedTab == 1
+                        ? ListView.builder(
+                            itemCount: 5,
+                            itemBuilder: (context, index) =>
+                                RecommendationDetailsCard(index: index + 1),
+                          )
+                        : ListView.builder(
+                            itemCount: 10,
+                            itemBuilder: (context, index) =>
+                                const RecommendationDetailsCard(),
+                          ),
                   ),
                 ],
-              ),
-            ),
-            Row(
-              children: [
-                TabOption(
-                  title: "Recommended",
-                  selectedTab: selectedTab == 1,
-                  onTap: () => setState(() => selectedTab = 1),
-                ),
-                TabOption(
-                  title: "All Menu Items",
-                  selectedTab: selectedTab == 2,
-                  onTap: () => setState(() => selectedTab = 2),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: selectedTab == 1
-                  ? ListView.builder(
-                      itemCount: 5,
-                      itemBuilder: (context, index) =>
-                          RecommendationDetailsCard(index: index + 1),
-                    )
-                  : ListView.builder(
-                      itemCount: 10,
-                      itemBuilder: (context, index) =>
-                          const RecommendationDetailsCard(),
-                    ),
-            ),
-          ],
+              );
+            }
+            return Container();
+          },
         ),
       ),
     );
